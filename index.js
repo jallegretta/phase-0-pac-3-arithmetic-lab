5232c1c4cd3435d2d22e15569e45ebc6b7615616
Jared <jaralleg@amazon.com>
2021-03-14 18:36:03 -0400

    math

---
+++ index.js
@@ -0,0 +1,48 @@
+let num1 = 10;
+let num2 = 5;
+let total = 0;
+let n = 10;
+
+function add(num1,num2) {
+    let total = num1 + num2;
+    return total;;
+}
+add();
+
+function subtract(num1,num2) {
+    let total = num1 - num2;
+    return total;
+}
+subtract();
+
+function multiply(num1,num2) {
+    let total = num1*num2;
+    return total;
+}
+multiply();
+
+function divide(num1,num2) {
+    let total = num1/num2;
+    return total;
+}
+divide();
+
+function increment(n) {
+    return n+= 1 ;
+}
+increment();
+
+function decrement(n) {
+    return n-= 1 ;
+    
+}
+decrement();
+
+function makeInt(n) {
+    return parseInt(n,10 )}
+makeInt();
+
+function preserveDecimal(n) {
+return parseFloat(n);
+}
+preserveDecimal();
